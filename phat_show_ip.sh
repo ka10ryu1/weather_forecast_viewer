1eadbce7e2683028809bbfbddb42116c5768c92a
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2021-05-01 10:24:33 +0900

    :art: utilディレクトリからアクセス

--- phat_show_ip.sh
+++ phat_show_ip.sh
@@ -4,4 +4,4 @@ IP_ADDR=$(ip a | grep -A 2 wlan0: | grep "inet 192.168." | sed 's/[\t ]\+/\t/g' 
 
 echo ${IP_ADDR}
 
-python3 /home/pi/Work/weather_forecast_viewer/phat_txt.py -n ${IP_ADDR}
+python3 /home/pi/Work/weather_forecast_viewer/util/phat_txt.py -n ${IP_ADDR}
