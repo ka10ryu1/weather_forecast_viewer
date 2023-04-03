be731969a710a486fc722a733f1b6fda17e91316
ryuichi.kato <lsyp.tuiovjo.1231@gmail.com>
2023-04-03 17:47:51 +0900

    :bug: スラッシュ忘れを修正

--- epd_4in01f/show_weather.sh
+++ epd_4in01f/show_weather.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GIT_DIR=/opt/weather_forecast_viewer
+GIT_DIR=/opt/weather_forecast_viewer/
 python3 $GIT_DIR/epd_4in01f_main.py \
   --api_key $GIT_DIR/weather_config.json \
   --symbol  $GIT_DIR/symbol \
